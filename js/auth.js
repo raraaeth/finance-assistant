349ec1f05f51cb7b485408153b95d9fcdd0d78b3
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-07-31 20:12:06 +0700

    Refactor Google Auth clientId and add authorization flow

--- js/auth.js
+++ js/auth.js
@@ -5,7 +5,7 @@
 
 const Auth = {
 
-    clientId: "http://768306848932-j88fhhepq1o6d2jr6itv5c1v8020uf3a.apps.googleusercontent.com",
+    clientId: "768306848932-j88fhhepq1o6d2jr6itv5c1v8020uf3a.apps.googleusercontent.com",
 
     redirectUri: "https://raraaeth.github.io/finance-assistant/auth/callback.html",
 
@@ -53,16 +53,105 @@ function isCallbackPage() {
 }
 
 
+
 /* ==========================================
    LOGIN
 ========================================== */
 
 function loginGoogle() {
 
-
+    requestAuthorization();
 
 }
 
+async function requestAuthorization() {
+
+    const verifier = await generateCodeVerifier();
+
+    const challenge = await generateCodeChallenge(verifier);
+
+    saveCodeVerifier(verifier);
+
+    const params = new URLSearchParams({
+
+        client_id: Auth.clientId,
+
+        redirect_uri: Auth.redirectUri,
+
+        response_type: "code",
+
+        scope: Auth.scopes.join(" "),
+
+        code_challenge: challenge,
+
+        code_challenge_method: "S256",
+
+        access_type: "offline",
+
+        prompt: "consent"
+
+    });
+
+    location.href =
+        "https://accounts.google.com/o/oauth2/v2/auth?"
+        + params.toString();
+
+}
+
+async function generateCodeVerifier() {
+
+    const random = new Uint8Array(32);
+
+    crypto.getRandomValues(random);
+
+    return base64UrlEncode(random);
+
+}
+
+function base64UrlEncode(buffer) {
+
+    return btoa(String.fromCharCode(...buffer))
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_")
+        .replace(/=/g, "");
+
+}
+
+async function generateCodeChallenge(verifier) {
+
+    const encoder = new TextEncoder();
+
+    const data = encoder.encode(verifier);
+
+    const hash = await crypto.subtle.digest("SHA-256", data);
+
+    return base64UrlEncode(new Uint8Array(hash));
+
+}
+
+function saveCodeVerifier(verifier) {
+
+    sessionStorage.setItem(
+        "code_verifier",
+        verifier
+    );
+
+}
+function getCodeVerifier() {
+
+    return sessionStorage.getItem(
+        "code_verifier"
+    );
+
+}
+
+function removeCodeVerifier() {
+
+    sessionStorage.removeItem(
+        "code_verifier"
+    );
+
+}
 
 /* ==========================================
    CALLBACK
