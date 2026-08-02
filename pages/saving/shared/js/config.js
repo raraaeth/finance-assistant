0859ac5476d3114ea66564f15de2580c322579a3
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-08-02 17:27:08 +0700

    Add configuration for hero badge and description

---
+++ pages/saving/shared/js/config.js
@@ -0,0 +1,17 @@
+/* =====================================================
+   CONFIG
+===================================================== */
+
+export const CONFIG = {
+
+    hero:{
+
+        badge:"Saving",
+
+        description:
+
+            "Wujudkan tujuan keuanganmu sedikit demi sedikit."
+
+    }
+
+};
