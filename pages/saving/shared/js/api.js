6d44622abd45bc2ad4f3d2fa32f85a937461b452
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-08-03 19:39:21 +0700

    Implement API object with load method
    Added API object with load function to fetch data.

--- pages/saving/shared/js/api.js
+++ pages/saving/shared/js/api.js
@@ -1,5 +1,68 @@
 /* =====================================================
-   API
+   Finance Assistant
+   Workspace   : Saving
+   Module      : API
+   File        : api.js
+   Version     : 1.0.0
+
+   Description :
+   OpenSheet API Engine
 ===================================================== */
 
-export const API = {};
+
+/* =====================================================
+   STATE
+===================================================== */
+
+export const API = {
+
+    raw : [],
+
+    bank : []
+
+};
+
+
+/* =====================================================
+   LOAD
+===================================================== */
+
+API.load = async function(
+
+    savingUrl,
+
+    bankUrl
+
+){
+
+    const [
+
+        saving,
+
+        bank
+
+    ] = await Promise.all([
+
+        fetch(
+
+            savingUrl
+
+        ),
+
+        fetch(
+
+            bankUrl
+
+        )
+
+    ]);
+
+    API.raw =
+
+        await saving.json();
+
+    API.bank =
+
+        await bank.json();
+
+};
