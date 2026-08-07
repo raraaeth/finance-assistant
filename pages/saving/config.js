e080bdd5f5bddcdb0f490970f831a48c9226ae73
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-08-07 20:43:02 +0700

    Add saving configuration in config.js
    This file contains configuration settings for the Saving page, including hero details and API endpoints.

---
+++ pages/saving/config.js
@@ -0,0 +1,60 @@
+/* =====================================================
+   Finance Assistant
+   Page        : Saving
+   Module      : Config
+   File        : config.js
+   Version     : 2.0.0
+
+   Description :
+   Saving Configuration
+
+   Sections :
+   - Hero
+   - API
+===================================================== */
+
+
+/* =====================================================
+   CONFIG
+===================================================== */
+
+export const CONFIG = {
+
+    /* =============================================
+       HERO
+    ============================================= */
+
+    hero : {
+
+        title :
+
+            "Saving",
+
+        description :
+
+            "Wujudkan tujuan keuanganmu sedikit demi sedikit.",
+
+        image :
+
+            "../assets/images/hero/hero-saving.png"
+
+    },
+
+
+    /* =============================================
+       API
+    ============================================= */
+
+    api : {
+
+        transaction :
+
+            "https://opensheet.elk.sh/1eVZV1BYpJlPGLiYWhd6C_kAoHZdbD-H7ykwAc1ddFiM/saving",
+
+        bank :
+
+            "https://opensheet.elk.sh/1eVZV1BYpJlPGLiYWhd6C_kAoHZdbD-H7ykwAc1ddFiM/saving_bank"
+
+    }
+
+};
