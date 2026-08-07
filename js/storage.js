e7a843d2a6e39c2fc64794d734e56a8944d6d896
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-08-07 11:53:37 +0700

    Refactor local storage functions in storage.js
    Refactor loadUser and saveUser functions for consistency.

--- js/storage.js
+++ js/storage.js
@@ -1,18 +1,108 @@
-const KEY = "finance-assistant";
+/* =====================================================
+   GLOBAL STORAGE
+   FILE : storage.js
+   DESCRIPTION : Local Storage Engine
+   VERSION : 2.0.0
 
-export function loadUser() {
+   Sections :
+   - Key
+   - User
+   - Workspace
+===================================================== */
+
+
+/* =====================================================
+   KEY
+===================================================== */
+
+const KEY =
+
+    "finance-assistant";
+
+
+/* =====================================================
+   USER
+===================================================== */
+
+export function loadUser(){
 
     return JSON.parse(
-        localStorage.getItem(KEY)
+
+        localStorage.getItem(
+
+            KEY
+
+        )
+
     );
 
 }
 
-export function saveUser(data){
+export function saveUser(
+
+    data
+
+){
 
     localStorage.setItem(
+
         KEY,
-        JSON.stringify(data)
+
+        JSON.stringify(
+
+            data
+
+        )
+
+    );
+
+}
+
+
+/* =====================================================
+   WORKSPACE
+===================================================== */
+
+export function loadWorkspace(){
+
+    return JSON.parse(
+
+        localStorage.getItem(
+
+            `${KEY}-workspace`
+
+        )
+
+    ) ?? {
+
+        app :
+
+            "saving",
+
+        workspace :
+
+            "saving"
+
+    };
+
+}
+
+export function saveWorkspace(
+
+    data
+
+){
+
+    localStorage.setItem(
+
+        `${KEY}-workspace`,
+
+        JSON.stringify(
+
+            data
+
+        )
+
     );
 
 }
