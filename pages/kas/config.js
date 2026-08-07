f0b1c6529319853b5c415a7906ef4f44668dccc5
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-08-07 21:21:03 +0700

    Add Kas configuration module
    This file contains configuration settings for the Kas module, including hero details and API endpoints.

---
+++ pages/kas/config.js
@@ -0,0 +1,60 @@
+/* =====================================================
+   Finance Assistant
+   Page        : Kas
+   Module      : Config
+   File        : config.js
+   Version     : 2.0.0
+
+   Description :
+   Kas Configuration
+
+   Sections :
+   - Hero
+   - API
+===================================================== */
+
+
+/* =====================================================
+   CONFIG
+===================================================== */
+
+export const CONFIG = {
+
+    /* =============================================
+       HERO
+    ============================================= */
+
+    hero : {
+
+        title :
+
+            "Kas",
+
+        description :
+
+            "Kelola kas bersama dengan mudah, transparan, dan terorganisir.",
+
+        image :
+
+            "../assets/images/hero/hero-saving.png"
+
+    },
+
+
+    /* =============================================
+       API
+    ============================================= */
+
+    api : {
+
+        transaction :
+
+            "https://opensheet.elk.sh/1eVZV1BYpJlPGLiYWhd6C_kAoHZdbD-H7ykwAc1ddFiM/kas",
+
+        member :
+
+            "https://opensheet.elk.sh/1eVZV1BYpJlPGLiYWhd6C_kAoHZdbD-H7ykwAc1ddFiM/kas_member"
+
+    }
+
+};
