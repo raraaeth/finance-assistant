835bea571766daf2827eff55de179198c1f1a16a
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-08-13 16:08:42 +0700

    Create summary.js for Payroll Daily Summary
    Added Payroll Daily Summary Controller with initialization method.

---
+++ pages/payroll-daily/summary.js
@@ -0,0 +1,34 @@
+/* =====================================================
+   Finance Assistant
+   Page        : Payroll Daily
+   Module      : Summary
+   File        : summary.js
+   Version     : 1.0.0
+
+   Description :
+   Payroll Daily Summary Controller
+===================================================== */
+
+
+/* =====================================================
+   STATE
+===================================================== */
+
+export const Summary = {
+
+
+    /* =================================================
+       INIT
+    ================================================= */
+
+    init(){
+
+        console.log(
+
+            "Payroll Daily Summary initialized"
+
+        );
+
+    }
+
+};
