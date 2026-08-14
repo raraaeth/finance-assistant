3be0cc1aab56eb5f6ac30751ca59d9c9ca4226d5
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-08-14 17:44:24 +0700

    Clean up statistics.js by removing unused code
    Removed commented-out code for showing the statistics page.

--- pages/payroll-daily/statistics.js
+++ pages/payroll-daily/statistics.js
@@ -92,34 +92,6 @@ export const Statistics = {
 Statistics.init = function(){
 
     /* =============================================
-       SHOW STATISTICS PAGE
-    ============================================= */
-
-    const page =
-
-        document.getElementById(
-
-            "statistics-page"
-
-        );
-
-
-    if(
-
-        page
-
-    ){
-
-        page.classList.remove(
-
-            "hidden"
-
-        );
-
-    }
-
-
-    /* =============================================
        FILTER
     ============================================= */
 
