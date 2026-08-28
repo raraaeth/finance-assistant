eb5ccbf6bb2adef33b0acb07972a791763c4e0fd
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-08-28 21:53:18 +0700

    Remove API configuration from config.js
    Removed API configuration and related comments from config.js.

--- pages/saving/config.js
+++ pages/saving/config.js
@@ -74,28 +74,6 @@ export const CONFIG = {
     },
 
 
-    /* =============================================
-       API
-
-       Menggunakan Global Finance Assistant API.
-
-       Endpoint ini menangani:
-
-       - Login
-       - Workspace
-       - Modules
-       - Create Module
-       - Read Module Sheet
-    ============================================= */
-
-    api : {
-
-        endpoint :
-
-            "https://script.google.com/macros/s/AKfycbxBiQSb1pioB0mDbkAqd6S3y4T5CTByn2-6kW7-T1l-5PdGYTBVDX4IXskxyu_QxokHDw/exec"
-
-    },
-
 
     /* =============================================
        DATA
