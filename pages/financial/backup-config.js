48365b13b5cffdd6bf6380ee70d28b76da3a5a9f
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-08-28 20:06:32 +0700

    Add financial configuration settings
    This file contains the financial configuration settings, including module details, hero information, API endpoint, and data structure.

---
+++ pages/financial/backup-config.js
@@ -0,0 +1,94 @@
+/* =====================================================
+   Finance Assistant
+   Page        : Financial
+   Module      : Config
+   File        : config.js
+   Version     : 2.0.0
+
+   Description :
+   Financial Configuration
+
+   Sections :
+   - Module
+   - Hero
+   - API
+   - Data
+===================================================== */
+
+
+/* =====================================================
+   CONFIG
+===================================================== */
+
+export const CONFIG = {
+
+
+    /* =============================================
+       MODULE
+    ============================================= */
+
+    module : {
+
+        key :
+
+            "financial",
+
+        name :
+
+            "Financial"
+
+    },
+
+
+    /* =============================================
+       HERO
+    ============================================= */
+
+    hero : {
+
+        title :
+
+            "Financial",
+
+        description :
+
+            "Pantau kondisi keuangan dan saldo aktualmu dengan lebih mudah.",
+
+        image :
+
+            "../assets/images/hero/hero-financial.png"
+
+    },
+
+
+    /* =============================================
+       API
+    ============================================= */
+
+    api : {
+
+        endpoint :
+
+            "https://script.google.com/macros/s/AKfycbxBiQSb1pioB0mDbkAqd6S3y4T5CTByn2-6kW7-T1l-5PdGYTBVDX4IXskxyu_QxokHDw/exec"
+
+    },
+
+
+
+    /* =============================================
+       DATA
+    ============================================= */
+
+    data : {
+
+        financial :
+
+            "financial",
+
+        activity :
+
+            "financial_activity"
+
+    }
+
+};
