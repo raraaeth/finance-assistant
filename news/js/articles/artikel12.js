2326f8a8e2dc7e775c28ea26f244aea0a203e06f
Raraa <153375584+raraaeth@users.noreply.github.com>
2026-09-24 21:06:13 +0700

    Add artikel12.js for finance assistant article

---
+++ news/js/articles/artikel12.js
@@ -0,0 +1,16 @@
+/* =====================================================
+   FINANCE ASSISTANT
+   NEWS & UPDATE
+
+   Article : 12
+   File    : artikel12.js
+
+   Title   :
+   Finance Assistant Adalah Apa?
+   Aplikasi untuk Membantu Mengelola Keuangan
+===================================================== */
+
+
+/* =====================================================
+   ARTICLE
+===================================================== */
